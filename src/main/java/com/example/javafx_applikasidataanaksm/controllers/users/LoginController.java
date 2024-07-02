8ca014f6ecd8d3fc15414e506f0a3d3b0cb6148d
ChristopherJ123 <christopherz2002@hotmail.com>
2024-07-03 01:38:15 +0700

    add login/register page

--- src/main/java/com/example/javafx_applikasidataanaksm/controllers/users/LoginController.java
+++ src/main/java/com/example/javafx_applikasidataanaksm/controllers/users/LoginController.java
@@ -1,11 +1,17 @@
 package com.example.javafx_applikasidataanaksm.controllers.users;
 
+import com.example.javafx_applikasidataanaksm.AppApplication;
 import com.example.javafx_applikasidataanaksm.DataAnakSMApplication;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
     @FXML
@@ -21,7 +27,7 @@ public class LoginController {
     }
 
     @FXML
-    private void handleLogin() {
+    private void handleLogin() throws IOException {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
@@ -31,6 +37,13 @@ public class LoginController {
             alert.setHeaderText(null);
             alert.setContentText("Welcome " + username);
             alert.showAndWait();
+
+            FXMLLoader appLoader = new FXMLLoader(DataAnakSMApplication.class.getResource("data_anak_sm-view.fxml"));
+            Scene scene = new Scene(appLoader.load());
+            Stage stage = new Stage();
+            stage.setTitle("Aplikasi Data Anak Sekolah Minggu");
+            stage.setScene(scene);
+            stage.show();
         } else {
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("Login Failed");
